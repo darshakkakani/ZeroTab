47850f1d79ef95937dddb50cf24a09395b29cdf1
darshakkakani <darshakkakani@users.noreply.github.com>
2026-06-16 11:59:35 +0000

    deploy: web build from c1edf50d99fd8418fa2b0029207834c9346be754 c1edf50d99fd8418fa2b0029207834c9346be754

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "709006678" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2275796326" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async function(engineInitializer) {
     // Use HTML renderer via bootstrap (--web-renderer flag removed in Flutter 3.22+).
